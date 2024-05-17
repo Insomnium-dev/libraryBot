dea234a90ef44cda969fadd8cc92adc509d5d80a
Daniil Haranski <wheconn@gmail.com>
2024-05-17 20:52:29 +0300

    Added a database connection

--- kb.py
+++ kb.py
@@ -5,6 +5,18 @@ import texts as tt
 from books import Book
 
 
+
+# some buttons
+btnBackBookList = types.InlineKeyboardButton(text=tt.back, callback_data="backToBooksList")
+
+
+# some functions
+def single_button(btn):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(btn)
+    return markup
+
+
 # Markups
 # /start buttons
 def get_markup_main():
@@ -16,13 +28,12 @@ def get_markup_main():
 
 def get_markup_signIn():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton(tt.signIn))
+    markup.add(types.KeyboardButton(text=tt.catalog))
     return markup
 
 
 def get_markup_books(books: list[Book]):
     markup = types.InlineKeyboardMarkup(resize_keyboard=True)
     for book in books:
-        markup.add(types.InlineKeyboardButton(text=f"{book.Id}. {book.Name}, Автор:{book.Author}, Жанр:{book.Genre}", callback_data=f""))
-        markup.add(types.InlineKeyboardButton(text=f"Цена:{book.Price}", callback_data=f""))
+            markup.add(types.InlineKeyboardButton(text=f"{book.Id}.\"{book.Name}\",\nАвтор:{book.Author}", callback_data=f"bookSelected_{book.Id}"))
     return markup
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import message, user
 from aiogram.utils.exceptions import MessageNotModified
 import kb
+import os
 import asyncio
 
 
@@ -15,7 +16,7 @@ import books
 conn = sqlite3.connect("data.db")
 c = conn.cursor()
 
-database.create_db()
+
 storage = MemoryStorage()
 bot = Bot(token="7049188608:AAHLPVz1skj5OGVuka88VAHwylSqJy6x0pM")
 dp = Dispatcher(bot, storage=storage)
@@ -25,7 +26,7 @@ dp = Dispatcher(bot, storage=storage)
 async def welcome(message: types.Message):
     await bot.send_message(
         chat_id=message.chat.id,
-        text="Добро пожаловать в книжный магазин!\nПожалуйста, авторизуйтесь.",
+        text=tt.greet,
         reply_markup=kb.get_markup_main(),
     )
 
@@ -34,7 +35,7 @@ async def handle_text(message):
     if message.text == tt.signIn:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text=tt.signIn,
+                text= "Вы зашли как пользователь!\nДля просмотра книг нажмите на кнопку ⬇️",
                 reply_markup=kb.get_markup_signIn(),
             )
     elif message.text == tt.signUp:
@@ -47,7 +48,7 @@ async def handle_text(message):
         books_list = books.get_books_list()
         await bot.send_message(
             chat_id=message.chat.id,
-            text=tt.catalog,
+            text=tt.catalog+":",
             reply_markup=kb.get_markup_books(books_list)
         )
     else:
@@ -58,14 +59,28 @@ async def process_callback(callback_query: types.CallbackQuery):
     chat_id = callback_query.message.chat.id
     call_data = callback_query.data
 
-    if call_data == "":
+    if call_data.startswith('bookSelected_'):
+        book_id = call_data.split('_')[1]
+        book = books.get_books_by_id(book_id)
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=callback_query.message.message_id,
-            text=tt.admin_panel,
-            reply_markup=kb.get_markup_main(),
+            text=tt.get_book_about(book),
+            reply_markup=kb.single_button(kb.btnBackBookList),
+        )
+
+    if call_data == 'backToBooksList':
+        books_list = books.get_books_list()
+        await bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=callback_query.message.message_id,
+            text=tt.catalog+":",
+            reply_markup=kb.get_markup_books(books_list)
         )
 
 
 if __name__ == '__main__':
+    if not os.path.exists('data.db'):
+        database.create_db()
+
     executor.start_polling(dp, skip_updates=True)
